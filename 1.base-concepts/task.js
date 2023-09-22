46b3433632b9b4ba610d27bf74e31f1d3ebcd1dd
ParaPaca <svetaalex8@gmail.com>
2023-09-22 15:53:00 +0300

    Оба задания готовы

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let d = (b ** 2) - (4 * a * c);
+
+	if (d < 0) {
+		return arr = [];
+	} else if (d === 0) {
+		arr.push(-b / (2 * a));
+		return arr;
+	} else if (d > 0) {
+		arr.push((-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a));
+		return arr;
+	}
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let P = (percent / 100) / 12;
+	let S = amount - contribution;
+
+	let result = S * (P + (P / (((1 + P) ** countMonths) - 1))) * countMonths;
+
+	return +result.toFixed(2);
 }

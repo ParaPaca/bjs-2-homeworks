36cf62c8c3e46aa291399df334ba1983392258f6
ParaPaca <svetaalex8@gmail.com>
2023-09-25 20:34:52 +0300

    Оба задания сделаны

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,20 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  return arr1.every((num, index) => num === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  if ((gender !== "мужской" && gender !== "женский") || users.length === 0)
+    return 0;
+
+  let result =
+    users
+      .filter((item) => item.gender == gender)
+      .reduce((sum, cur) => (sum += cur.age), 0) /
+    users.filter((item) => item.gender == gender).length;
+
+  return result;
 }
